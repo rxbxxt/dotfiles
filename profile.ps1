4ab2c7587654928427112608ef6f9b26a27faa5b
Miyake Kichiro <nshyahn1@gmail.com>
2023-03-17 16:04:32 +0200

    Aliases

--- profile.ps1
+++ profile.ps1
@@ -7,6 +7,18 @@ oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\multiverse-neon.omp.json" |
 # Install-Module -Name Terminal-Icons -Repository PSGallery
 Import-Module -Name Terminal-Icons
 
+
+# Aliases
+Set-Alias vi nvim
+
 function fun {
   Set-Location D:\fun\
+}
+
+function vicnf {
+  vi $ENV:USERPROFILE\AppData\Local\nvim\init.vim
+}
+
+function pscnf {
+  vi $PROFILE
 }
