20347d565bd5cfcb83acab75e0d9ddf153b73de2
Miyake Kichiro <nshyahn1@gmail.com>
2023-03-17 15:25:35 +0200

    Windoos colors

--- profile.ps1
+++ profile.ps1
@@ -1,2 +1,8 @@
 Set-PSReadLineOption -EditMode Emacs
 Set-Location D:\fun\
+
+# Install nerd font
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\multiverse-neon.omp.json" | Invoke-Expression
+
+# Install-Module -Name Terminal-Icons -Repository PSGallery
+Import-Module -Name Terminal-Icons
